625a2f578128485ce71a2480811e3afa95eca915
mafeLopez26 <149280498+mafeLopez26@users.noreply.github.com>
2024-03-20 16:55:56 -0500

    Add files via upload

---
+++ 01.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Registrar cliente");
+                Console.WriteLine("2. Salir");
+                Console.Write("Seleccione una opción: ");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        RegistrarCliente();
+                        break;
+                    case 2:
+                        Console.WriteLine("Saliendo del programa...");
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, seleccione nuevamente.");
+                        break;
+                }
+            }
+        }
+
+        static void RegistrarCliente()
+        {
+            Cuenta cuenta = new Cuenta();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Menú de cuenta:");
+                Console.WriteLine("1. Acreditar saldo");
+                Console.WriteLine("2. Cargar saldo");
+                Console.WriteLine("3. Obtener saldo actual");
+                Console.WriteLine("4. Volver al menú principal");
+                Console.Write("Seleccione una opción: ");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese el monto a acreditar: ");
+                        int montoCredito = int.Parse(Console.ReadLine());
+                        cuenta.Credit(montoCredito);
+                        Console.WriteLine("Saldo acreditado exitosamente.");
+                        break;
+                    case 2:
+                        Console.Write("Ingrese el monto a cargar: ");
+                        int montoCarga = int.Parse(Console.ReadLine());
+                        cuenta.Cargar(montoCarga);
+                        break;
+                    case 3:
+                        Console.WriteLine("Saldo actual de la cuenta: " + cuenta.ObtenerSaldo());
+                        break;
+                    case 4:
+                        Console.WriteLine("Volviendo al menú principal...");
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, seleccione nuevamente.");
+                        break;
+                }
+            }
+        }
+
+        class Cuenta
+        {
+            private int saldo = 0;
+            private int monto = 0;
+
+            public Cuenta()
+            {
+                saldo = 0;
+            }
+
+            public void Credit(int monto)
+            {
+                this.monto = monto;
+                saldo += monto;
+            }
+            public void Cargar(int monto)
+            {
+                this.monto = monto;
+                if (monto > saldo)
+                {
+                    Console.WriteLine("El monto a cargar excede el saldo de la cuenta");
+                }
+                else
+                {
+                    saldo -= monto;
+                    Console.WriteLine("Saldo cargado exitosamente.");
+                }
+            }
+            public int ObtenerSaldo()
+            {
+                return saldo;
+            }
+        }
+    }
+}
---
+++ 02.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Punto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Factura factura = null;
+
+            int opcion;
+
+            while (true)
+            {
+                Console.WriteLine("MENU:");
+                Console.WriteLine("1. Crear nueva factura");
+                Console.WriteLine("2. Mostrar factura actual");
+                Console.WriteLine("3. Salir");
+                Console.Write("Seleccione una opción: ");
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el número de pieza:");
+                        string numeroPieza = Console.ReadLine();
+
+                        Console.WriteLine("Ingrese la descripción de la pieza:");
+                        string descripcionPiezas = Console.ReadLine();
+
+                        Console.WriteLine("Ingrese la cantidad de piezas:");
+                        int cantidadPiezas = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Ingrese el precio por artículo:");
+                        int precioArticulo = Convert.ToInt32(Console.ReadLine());
+
+                        factura = new Factura(numeroPieza, descripcionPiezas, cantidadPiezas, precioArticulo);
+
+                        Console.WriteLine("¡Factura creada con éxito!");
+                        break;
+
+                    case 2:
+
+                        if (factura != null)
+                        {
+                            Console.WriteLine("INFORMACIÓN DE LA FACTURA:");
+                            Console.WriteLine("Número de pieza: " + factura.ObtenerNumeroPieza());
+                            Console.WriteLine("Descripción de piezas: " + factura.ObtenerDescripcionPiezas());
+                            Console.WriteLine("Cantidad de piezas: " + factura.ObtenerCantidadPiezas());
+                            Console.WriteLine("Precio por artículo: " + factura.ObtenerPrecioArticulo());
+                            Console.WriteLine("Monto de la factura: " + factura.obtenerMontoFactura());
+                        }
+                        else
+                        {
+                            Console.WriteLine("No hay factura creada.");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("¡Hasta luego!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+        }
+
+        class Factura
+        {
+            private string numeroPieza;
+            private string descripcionPiezas;
+            private int cantidadPiezas = 0;
+            private int precioarticulo = 0;
+
+            public Factura(string numeroPieza, string descripcionPiezas, int cantidadPiezas, int precioarticulo)
+            {
+                this.numeroPieza = numeroPieza;
+                this.descripcionPiezas = descripcionPiezas;
+                this.cantidadPiezas = cantidadPiezas;
+                this.precioarticulo = precioarticulo;
+            }
+
+            public void EstablecerNumeroPieza(string numeroPieza)
+            {
+                this.numeroPieza = numeroPieza;
+            }
+
+            public string ObtenerNumeroPieza()
+            {
+                return numeroPieza;
+            }
+
+            public void EstablecerDescripcionPiezas(string descripcionPiezas)
+            {
+                this.descripcionPiezas = descripcionPiezas;
+            }
+
+            public string ObtenerDescripcionPiezas()
+            {
+                return descripcionPiezas;
+            }
+
+            public void EstablecerCantidadPiezas(int cantidadPiezas)
+            {
+                this.cantidadPiezas = cantidadPiezas;
+            }
+
+            public int ObtenerCantidadPiezas()
+            {
+                return cantidadPiezas;
+            }
+
+            public void EstablecerPrecioArticulo(int precioarticulo)
+            {
+                this.precioarticulo = precioarticulo;
+            }
+
+            public int ObtenerPrecioArticulo()
+            {
+                return precioarticulo;
+            }
+
+            public int obtenerMontoFactura()
+            {
+                return cantidadPiezas * precioarticulo;
+            }
+        }
+    }
+}
